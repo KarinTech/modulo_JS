a067d42a7b1db08eff53524eaf6c6d77b7a7c24c
Karin Centenaro <98807488+KarinTech@users.noreply.github.com>
2022-07-26 05:59:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 
 let nombre="Karin";
 nombre="Brenda";
-let pais ="argentina";
+let pais ="Argentina";
 let edad= 25;
 console.log("Mi nombre es " +nombre + "y tengo " +edad + "anos" + "y soy de " + pais  )
 
-let prompt("Ingrese un numero")
-console.log =parseInt(numero- numero2)
+
+let a= prompt("Ingrese un numero")
+let b= prompt("Ingrese otro numero")
+let calculo = parseInt(a) + parseInt(b);
+console.log = (calculo)
+  
