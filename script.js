408bf049f7ae0ad8e7492afd3f50aae41c977e66
Karin Centenaro <98807488+KarinTech@users.noreply.github.com>
2022-07-26 05:52:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+let nombre="Karin";
+nombre="Brenda";
+let pais ="argentina";
+let edad= 25;
+console.log("Mi nombre es " +nombre + "y tengo " +edad + "anos" + "y soy de " + pais  )
+
+let prompt("Ingrese un numero")
+console.log =parseInt(numero- numero2)
